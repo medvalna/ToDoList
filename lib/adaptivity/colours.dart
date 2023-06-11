90dde75befb5d71cef78fcd672219c079f308b06
Valentina <v.m.30.03.20@gmail.com>
2023-06-11 20:21:19 +0300

    added global colors and font sizes

---
+++ lib/adaptivity/colours.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color maintext = Color(0xFF000000);
+const Color secondarytext = Color(0x4D000000);
+const Color back_light = Color(0xFFF7F6F2);
+const Color tileback_light = Color(0xFFFFFFFF);
+const Color add = Color(0xFF007AFF);
---
+++ lib/adaptivity/font_sizes.dart
@@ -0,0 +1,5 @@
+const double largetitle = 38;
+const double title = 32;
+const double button = 24;
+const double body= 20;
+const double subhead = 20;
